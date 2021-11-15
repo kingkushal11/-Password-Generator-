65ac067b2f0aa57a0bf8e7188d7ac76e8f512e47
Kushal Khattri <kushalk62@gmail.com>
2021-11-15 15:18:05 -0800

    added letters and fixed password

--- script.js
+++ script.js
@@ -10,30 +10,46 @@ function writePassword() {
 
 }
 function generatePassword(){
-  var choosenletters = ""
-  var uppercaseletters = "ABCD"
-  var lowercaseletters = "abcd"
-  var numericletters = "1234"
-  var specialletters = "!@#$" 
-  var length = prompt("What is the length of the password?")
+  var choosenletters = "";
+  var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseletters = "abcdefghijklmnopqrstuvwxyz";
+  var numericletters = "1234567890";
+  var specialletters = "!@#$%^&*()_+{}|:?><,./';[]=-";
+ 
+  var length = prompt("What is the length of the password?");
   console.log(length);
+  
   if (length < 8){
     return "Too Short";
  }
   if (length > 128){
     return "Too Long";
   } 
+  
   var uppercase = confirm("Do you want uppercase letter.");
+  if (uppercase === true){
+   choosenletters = choosenletters+uppercaseletters;
+  }
   var lowercase = confirm("Do you want lowercase letter.");
+  if (lowercase === true){
+    choosenletters = choosenletters+lowercaseletters;
+  }
   var numeric = confirm("Do you want numeric.");
+  if (numeric === true){
+    choosenletters = choosenletters+numericletters;
+  }
   var special = confirm("Do you want special characters.");
+  if (special === true){
+    choosenletters = choosenletters+specialletters;
+  }
+ 
   console.log(uppercase, lowercase, numeric, special);
   var pw = ""
 
   for(var i = 0; i < length; i++){
    
-   var randomIndex = Math.floor(Math.random() * uppercaseletters.length); 
-   var randomElement = uppercaseletters[randomIndex];
+   var randomIndex = Math.floor(Math.random() * choosenletters.length); 
+   var randomElement = choosenletters[randomIndex];
    pw = pw + randomElement
   
   }
@@ -43,3 +59,4 @@ function generatePassword(){
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
